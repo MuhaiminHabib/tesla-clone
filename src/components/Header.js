9c9cfb17a83d0d29788c60fd90e8e9a4306f7e55
MuhaiminHabib <muhamin.habib077@gmail.com>
2022-05-01 12:59:06 +0600

    working on the hamburger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Header = ({title}) => {
   return (
@@ -17,10 +18,21 @@ const Header = ({title}) => {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <CustomMenu>
-
-        </CustomMenu>
+        <CustomMenu />
       </RightMenu>
+      <BurgerMenu>
+        <CloseWrapper>
+          <CustomClose />
+        </CloseWrapper>
+        <li><a herf="#">Existing Inventory</a></li>
+        <li><a herf="#">Used Inventory</a></li>
+        <li><a herf="#">Trade-in</a></li>
+        <li><a herf="#">Cybertruck</a></li>
+        <li><a herf="#">Roadster</a></li>
+        <li><a herf="#">Existing Inventory</a></li>
+        <li><a herf="#">Existing Inventory</a></li>
+        <li><a herf="#">Existing Inventory</a></li>
+      </BurgerMenu>
     </Container>
   )
 }
@@ -33,10 +45,12 @@ const  Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
   top: 0;
   left: 0;
   right: 0;
+  z-index: 1;
 `
 
 const Menu = styled.div`
@@ -52,9 +66,15 @@ const Menu = styled.div`
     padding: 0 10px;
     flex-wrap: no-wrap;
   }
+
+  @media (max-width: 786px) {
+    display: none;
+  }
 `
 
 const RightMenu = styled.div`
+  display: flex;
+  align-items: center;
   a {
     font-weight: 600;
     text-transform: uppercase;
@@ -62,4 +82,39 @@ const RightMenu = styled.div`
   }
 `
 
-const CustomMenu = styled(MenuIcon)``
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
+`
+
+const BurgerMenu = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background: white;
+  width: 300px;
+  z-index: 16;
+  list-style: none;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: start;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2)
+
+    a {
+      font-weight: 600;
+    }
+  }
+`
+const CloseWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const CustomClose = styled(CloseIcon)`
+  cursor: pointer;
+`
+
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Section from './Section';
 
 
+
 const Home = () => {
     return (
         <Container>
